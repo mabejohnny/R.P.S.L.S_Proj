dc3ea9fb6daee37220062aaa56917e9d1cfbb0d9
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-19 23:22:19 -0400

    LargeUpdate.js
    Wrote a TON of code today.  Started this morning by trying to do this project using just the teq ive learned so far.  I took that and translated it into OOP.  Still trying to grasp the understanding 100%, but im getting there!
    
    created : player class and game class.
    
    I used inheritence with my computer class which is a child of parent player
    
    I finished up a switchCase which lets the user choose their gesture.
    
    I finished an array that includes all the gestures, which the random computer function pulls one and returns a random.
    
    finished displayed rules
    
    finished user name prompt (not necessary, but it will add a custom touch throughtout with the users name.

--- spockGame.js
+++ spockGame.js
@@ -3,14 +3,17 @@
 //prompt asking whether the user is playing single or multiplayer (switchcase for multiplayer with .random for single)
 
 //single player game:
-
+//welcome! plus ask for name to pass down
 //display the rules 
-//prompt switchcase with choices (rock,paper,spock)
+//prompt choices (rock,paper,spock)
+//computer random choice 
+//compare user choice/random choice
+//whoever wins that rount alert
+//add counter for winner
+//loop back and do it again
+//best of three
 //counter for 1 point added for each win
-//best of three
-//compare the two totals of each player
-//higher total gets a point
-//best of three
+
 
 //multiplayer:
 
@@ -23,24 +26,6 @@
 //compare the two totals of each player
 //higher total gets a point
 //best of three
-
-//Get the computers choice
-
-//let computerChoice = Math.random();
-
-//if (computerChoice < 0.2) {
-//    computerChoice = "rock";
-//} else if (computerChoice <= 0.4) {
-//    computerChoice = "paper";
-//} else if (computerChoice <= 0.6) {
-//    computerChoice = "scissors";
-//} else if (computerChoice <= 0.8) {
-//    computerChoice = "lizard";
-//} else {
-//    computerChoice = "spock";
-//}
-
-
 
 
 
@@ -60,15 +45,6 @@
 //}
 
 
-//function playerNameInputSingle() {
-//    let userNameInput = prompt("Please enter your name:")
-//        alert("Welcome to the game " + userNameInput + "!")
-
-//        runGameSingle(userNameInput);
-
-//}
-
-
 //function playerNameInputMultiPlayer() {
 //    let userNameInputPlayerOne = prompt("Enter player '1' name:");
 //    let userNameInputPlayerTwo = prompt("Enter player '2' name:");
@@ -77,11 +53,6 @@
 //        runGameMultiPlayer(userNameInputPlayerOne, userNameInputPlayerTwo);
 //}
 
-//function displayRulesSinglePlayer(userNameInput) {
-//    alert("Welcome to the classic game: + \n Rock Paper Scissors Lizard Spock!" + "\n" + userNameInput +"will go first" + "The computer will go second" + "\n" + "Each round will be worth one point" + "The first player to win 2 out of 3 rounds will win the game!");
-
-//    alert("Remember! " + "\n" + "Rock crushes Scissor" + "\n" + "Scissor cuts Paper"+ "\n" + "Paper covers Rock"+ "\n" + "Rock crushes Lizard"+ "\n" + "Lizard poisons Spock"+ "\n" + "Spock Smashes Scissor"+ "\n" + "Scissor decapitates Lizard"+ "\n" + "Lizard eats Paper"+ "\n" + "Paper disproves Spock"+ "\n" + "Spock vaporizes Rock")
-
 //function displayRulesMultiPlayer(userNameInputPlayerOne, userNameInputPlayerTwo) {
 //      console.log("Welcome to the classic game: + \n Rock Paper Scissors Lizard Spock!");
 //    console.log(userNameInputPlayerOne + "will go first! " + "\n" + userNameInputPlayerTwo + "will go second!");
@@ -89,7 +60,7 @@
 //    console.log("The first player to win 2 out of 3 rounds will win the game");
 //  console.log("Remember! " + "\n" + "Rock crushes Scissor" + "\n" + "Scissor cuts Paper"+ "\n" + "Paper covers Rock"+ "\n" + "Rock crushes Lizard"+ "\n" + "Lizard poisons Spock"+ "\n" + "Spock Smashes Scissor"+ "\n" + "Scissor decapitates Lizard"+ "\n" + "Lizard eats Paper"+ "\n" + "Paper disproves Spock"+ "\n" + "Spock vaporizes Rock")
 
-//        this.choices = [{
+//        choices = [{
 //            "choice": "rock",
 //            "beats": ["scissors, lizard"],
 //            "losesTo": ["paper, Spock"],
@@ -113,44 +84,112 @@
 
 //    }
 
+
+
+
+
+
 class Game {
-    constructor(){
-    this.playerOne = new Player(userNameInputPlayerOne);
+    constructor(userNameInput){
+    this.playerOne = new Player(userNameInput);
     this.PlayerTwo = new Player("Computer");
 
-        this.Choices = [];
+}
 
-        this.choices.push(new Gestures("rock"));
-        this.choices.push(new Gestures("paper"));
-        this.choices.push(new Gestures("scissors"));
-        this.choices.push(new Gestures("lizard"));
-        this.choices.push(new Gestures("spock"));
 
-       
+runGame() { //"main" method
+    this.playerNameInputSingle();
+    this.displayRulesSinglePlayer(userNameInput);
+    this.chooseTheGestureToBeUsed(userNameInput)
 
+}
+
+
+playerNameInputSingle() {
+    let userNameInput = prompt("Welcome to the classic game: + \n Rock Paper Scissors Lizard Spock!" + "\n" + "Please enter your name:")
+        alert("Welcome to the game " + userNameInput + "!")
+
+}
+
+
+displayRulesSinglePlayer(userNameInput) {
+         alert("Welcome to your own worst nightmare " + userNameInput + "!" + "\n" +"You will go first, and the computer second" + "\n" + "- Each round will be worth one point" + "\n" + "- The first player to win 2 out of 3 rounds will win the game!");
+    
+         alert("Remember! " + "\n" + "Rock crushes Scissor" + "\n" + "Scissor cuts Paper"+ "\n" + "Paper covers Rock"+ "\n" + "Rock crushes Lizard"+ "\n" + "Lizard poisons Spock"+ "\n" + "Spock Smashes Scissor"+ "\n" + "Scissor decapitates Lizard"+ "\n" + "Lizard eats Paper"+ "\n" + "Paper disproves Spock"+ "\n" + "Spock vaporizes Rock")
+   
     }
 
 }
+
 
 class Player {
-    constructor(userNameInput){
+    constructor(name){
         this.score = 0;
-        this.userNameInput = userNameInput;
+        this.name = name;
+        this.typeOfGestures = this.typeOfGestures
+        this.Gestures=[new Gestures("rock"), new Gestures("paper"), new Gestures("scissors"), new Gestures("lizard"), new Gestures("spock")]   
+    
     }
+    
+    
+    generateRandomGestureForComputer() {
+        let randomGesture = this.Gestures[Math.floor(Math.random() * this.Gestures.length)];{
+        return randomGesture;
 
     }
-  
+}
 
-class Gestures {
-    constructor(typeOfGensture){
-        this.typeOfGensture = this.typeOfGensture
+    chooseTheGestureToBeUsed() {
+        let userGestureAlert = prompt(userNameInput + "!"+ "Choose your weapon!!!" + "\n" + "TYPE: 1 for 'rock' " + "\n" + "TYPE: 2 for 'paper' " + "\n" + "TYPE: 3 for 'scissor' " + "\n" + "TYPE: 4 for 'lizard' " + "\n" + "TYPE: 5 for 'spock' " + "\n" + "\n" + "* To return to the main menu, TYPE: 6 *");
+        let userGestureChoice;
+        
+        switch(userGestureChoice)
+        {
+            case "1":
+                let theyPickedRock  = letsDoTheMathToFindAWinner(userGestureChoice);
+                break;
+            case "2":
+                let theyPickedPaper  = letsDoTheMathToFindAWinner(userGestureChoice);
+                break;
+            case "3":
+                let theyPickedscissor  = letsDoTheMathToFindAWinner(userGestureChoice);
+                break;
+            case "4":
+                let theyPickedLizard  = letsDoTheMathToFindAWinner(userGestureChoice);
+                break;
+            case "5":
+                let theyPickedSpock  = letsDoTheMathToFindAWinner(userGestureChoice);
+                break;
+            case "6":
+                //return to main menu
+            default:
+                alert("Please try again!");
+                this.chooseTheGestureToBeUsed();
+    
+    
+        }
     }
+            
+}
+
+class Computer extends Player {
+    constructor(name){
+    super(name);
+
+    }
+
+    letsDoTheMathToFindAWinner(userGestureChoice, randomGesture) {
+
+
+    }
+
+
 
 }
-  
-let game = new Game();
 
-console.log('game', game);
+
+let game = new Game();
+game.runGame();
 
 
 
