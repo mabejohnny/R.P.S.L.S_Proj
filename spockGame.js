ed9b720202248439d7614c2b07fa03fcae26a5f1
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-20 16:59:56 -0400

    cleanUp.js
    -cleaned up and spaced out some code.
    
    -added some alerts into generate random number for computer function.
    
    - started adding if/else statments for the users choice vs the computers choice.  Im now thinking of a way to create an array with the choices, who they beat and who they loose to, then run a 4 loop around the whole thing and just have it go through and compare

--- spockGame.js
+++ spockGame.js
@@ -90,32 +90,37 @@
 
 
 class Game {
-    constructor(userNameInput){
-    this.playerOne = new Player(userNameInput);
-    this.PlayerTwo = new Player("Computer");
+    constructor(theUsersName){
+
+        this.playerOne = new Player(theUsersName);
+        this.PlayerTwo = new Player("Computer");
 
 }
 
 
-runGame() { //"main" method
-    this.playerNameInputSingle();
-    this.displayRulesSinglePlayer(userNameInput);
-    this.chooseTheGestureToBeUsed(userNameInput)
+    runGame() { //"main" method
+
+        this.getTheSinglePlayersName();
+        this.displayRulesForSinglePlayer(theUsersName);
+        this.userChoosesTheirGesture(theUsersName)
 
 }
 
 
-playerNameInputSingle() {
-    let userNameInput = prompt("Welcome to the classic game: + \n Rock Paper Scissors Lizard Spock!" + "\n" + "Please enter your name:")
-        alert("Welcome to the game " + userNameInput + "!")
+    getTheSinglePlayersName() {
+
+        let theUsersName = prompt("Welcome to the classic game: + \n Rock Paper Scissors Lizard Spock!" + "\n" + "Please enter your name:")
+        
+        alert("Welcome to the game " + theUsersName + "!")
 
 }
 
 
-displayRulesSinglePlayer(userNameInput) {
-         alert("Welcome to your own worst nightmare " + userNameInput + "!" + "\n" +"You will go first, and the computer second" + "\n" + "- Each round will be worth one point" + "\n" + "- The first player to win 2 out of 3 rounds will win the game!");
+    displayRulesForSinglePlayer(theUsersName) {
+
+        alert("Welcome to your own worst nightmare " + theUsersName + "!" + "\n" +"You will go first, and the computer second" + "\n" + "- Each round will be worth one point" + "\n" + "- The first player to win 2 out of 3 rounds will win the game!");
     
-         alert("Remember! " + "\n" + "Rock crushes Scissor" + "\n" + "Scissor cuts Paper"+ "\n" + "Paper covers Rock"+ "\n" + "Rock crushes Lizard"+ "\n" + "Lizard poisons Spock"+ "\n" + "Spock Smashes Scissor"+ "\n" + "Scissor decapitates Lizard"+ "\n" + "Lizard eats Paper"+ "\n" + "Paper disproves Spock"+ "\n" + "Spock vaporizes Rock")
+        alert("Remember! " + "\n" + "Rock crushes Scissor" + "\n" + "Scissor cuts Paper"+ "\n" + "Paper covers Rock"+ "\n" + "Rock crushes Lizard"+ "\n" + "Lizard poisons Spock"+ "\n" + "Spock Smashes Scissor"+ "\n" + "Scissor decapitates Lizard"+ "\n" + "Lizard eats Paper"+ "\n" + "Paper disproves Spock"+ "\n" + "Spock vaporizes Rock")
    
     }
 
@@ -124,68 +129,145 @@ displayRulesSinglePlayer(userNameInput) {
 
 class Player {
     constructor(name){
+
         this.score = 0;
         this.name = name;
         this.typeOfGestures = this.typeOfGestures
         this.Gestures=[new Gestures("rock"), new Gestures("paper"), new Gestures("scissors"), new Gestures("lizard"), new Gestures("spock")]   
+        this.
     
     }
-    
-    
-    generateRandomGestureForComputer() {
-        let randomGesture = this.Gestures[Math.floor(Math.random() * this.Gestures.length)];{
-        return randomGesture;
+    //             "choice": "rock",
+    //            "beats": ["scissors, lizard"],
+    //            "losesTo": ["paper, Spock"],
+    //            },{
+    //            "choice": "paper",
+    //            "beats": ["rock, spock"],
+    //            "losesTo": ["scissors, lizard"],
+    //            },{
+    //             "choice": "scissors",
+    //            "beats": ["paper, lizard"],
+    //            "losesTo": ["rock, spock"],
+    //            },{
+    //            "choice": "lizard",
+    //            "beats": ["spock, paper"],
+    //            "losesTo": ["rock, scissors"],
+    //           },{
+    //            "choice": "spock",
+    //            "beats": ["scissor, rock"],
+    //            "losesTo": ["lizard, paper"],
+    //            }];
 
-    }
-}
+        userChoosesTheirGesture() {
 
-    chooseTheGestureToBeUsed() {
-        let userGestureAlert = prompt(userNameInput + "!"+ "Choose your weapon!!!" + "\n" + "TYPE: 1 for 'rock' " + "\n" + "TYPE: 2 for 'paper' " + "\n" + "TYPE: 3 for 'scissor' " + "\n" + "TYPE: 4 for 'lizard' " + "\n" + "TYPE: 5 for 'spock' " + "\n" + "\n" + "* To return to the main menu, TYPE: 6 *");
-        let userGestureChoice;
+            let usersGestureChoice;
+            let usersGestureChoice = prompt(theUsersName + "!"+ "Choose your weapon!!!" + "\n" + "TYPE: 1 for 'rock' " + "\n" + "TYPE: 2 for 'paper' " + "\n" + "TYPE: 3 for 'scissor' " + "\n" + "TYPE: 4 for 'lizard' " + "\n" + "TYPE: 5 for 'spock' " + "\n" + "\n" + "* To return to the main menu, TYPE: 6 *");
         
-        switch(userGestureChoice)
-        {
-            case "1":
-                let theyPickedRock  = letsDoTheMathToFindAWinner(userGestureChoice);
-                break;
-            case "2":
-                let theyPickedPaper  = letsDoTheMathToFindAWinner(userGestureChoice);
-                break;
-            case "3":
-                let theyPickedscissor  = letsDoTheMathToFindAWinner(userGestureChoice);
-                break;
-            case "4":
-                let theyPickedLizard  = letsDoTheMathToFindAWinner(userGestureChoice);
-                break;
-            case "5":
-                let theyPickedSpock  = letsDoTheMathToFindAWinner(userGestureChoice);
-                break;
-            case "6":
-                //return to main menu
-            default:
-                alert("Please try again!");
-                this.chooseTheGestureToBeUsed();
-    
-    
+            switch(usersGestureChoice)
+            {
+                case "1":
+                    let theyPickedRock  = letsDoTheMathToFindAWinner(userGestureChoice);
+                    break;
+                case "2":
+                    let theyPickedPaper  = letsDoTheMathToFindAWinner(usersGestureChoice);
+                    break;
+                case "3":
+                    let theyPickedscissor  = letsDoTheMathToFindAWinner(usersGestureChoice);
+                    break;
+                case "4":
+                    let theyPickedLizard  = letsDoTheMathToFindAWinner(usersGestureChoice);
+                    break;
+                case "5":
+                    let theyPickedSpock  = letsDoTheMathToFindAWinner(usersGestureChoice);
+                    break;
+                case "6":
+                    //return to main menu (make sure its the main menu which prompts single or multiplayer)
+                default: 
+                    alert("Please try again!");
+                    this.userChoosesTheirGesture();
         }
+
     }
             
 }
+
 
 class Computer extends Player {
     constructor(name){
     super(name);
 
-    }
+}
 
-    letsDoTheMathToFindAWinner(userGestureChoice, randomGesture) {
+        generateRandomGestureForComputer() {
 
+            let randomGestureForComputer = this.Gestures[Math.floor(Math.random() * this.Gestures.length)];{
+    
+            return randomGestureForComputer;
 
-    }
-
-
+    }   
 
 }
+
+        //users name is getting passed down to personalize the alert, 
+        letsDoTheMathToFindAWinner(usersGestureChoice, randomGestureForComputer, theUsersName) {
+
+            alert( "are you ready" + theUsersName + "?!?!".toUpperCase() );
+            alert("Rock...Paper...Scissor...Lizard...Spock...SAYS..." + "\n" + "\n" + "\n" + "Press 'ok' to SHOOT!") 
+            
+            let theWinner = [];
+            if(parents.length == 0){
+              return siblings;
+            }
+          
+            else if(parents.length >= 1){ 
+              siblings = people.filter(function (el){
+                
+                if(el.parents.length == 0 || el === person){
+                  return false;
+                }
+                else if(el.parents.length == 1 && parents.length == 1){
+                  if(el.parents[0] === parents[0].id){
+                    return true;
+                  }
+                  else{
+                    return false;
+                  }
+                }
+                else if(el.parents.length == 2 && parents.length == 1){
+                  if(el.parents[0] === parents[0].id || el.parents[1] === parents[0].id){
+                    return true;
+                  }
+                  else{
+                    return false;
+                  }
+                }
+                else if(el.parents.length == 1 && parents.length == 2){
+                  if(el.parents[0] === parents[0].id || el.parents[0] === parents[1].id){
+                    return true;
+                  }
+                  else{
+                    return false;
+                  }
+                }  
+                else if(el.parents.length == 2 && parents.length == 2){
+                  if(el.parents[0] === parents[0].id || el.parents[0] === parents[1].id || el.parents[1] === parents[0].id || el.parents[1] === parents[1].id){
+                      return true;
+                    }
+                    else{
+                      return false;
+                    }
+                }
+              })
+            }   
+            return siblings;
+          }
+          
+}
+
+
+
+
+
 
 
 let game = new Game();
