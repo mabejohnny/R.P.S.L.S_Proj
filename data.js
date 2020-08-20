53c0f9beb35b26e78743660b2370341a0fefaddf
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-08-20 17:10:04 -0400

    addedInfoToDate.js
    added the info im trying to capture to the data.js page.
    
    now have to see how i am going to pull from it.  thinking something similar to the most wanted proj

---
+++ data.js
@@ -0,0 +1,27 @@
+const data = [
+	{
+		"choice": "rock",
+        "beats": ["scissors, lizard"],
+        "losesTo": ["paper, Spock"],
+	},
+	{
+        "choice": "paper",
+        "beats": ["rock, spock"],
+        "losesTo": ["scissors, lizard"]
+	},
+	{
+        "choice": "scissors",
+        "beats": ["paper, lizard"],
+        "losesTo": ["rock, spock"],
+	},
+	{
+        "choice": "lizard",
+        "beats": ["spock, paper"],
+        "losesTo": ["rock, scissors"],   
+	},
+	{
+        "choice": "spock",
+        "beats": ["scissor, rock"],
+        "losesTo": ["lizard, paper"],
+    }
+];
